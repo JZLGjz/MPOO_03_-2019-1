ac71cee5caf0f1066fa1712875bce21ab33e4a0e
JZLGjz <42215210+JZLGjz@users.noreply.github.com>
2018-11-08 12:48:09 -0600

    Add files via upload

---
+++ domoticaPF/domoticaPF/HabitacionViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HabitacionViewController.swift
+//  domoticaPF
+//
+//  Created by Macbook on 11/7/18.
+//  Copyright © 2018 iosdevlab. All rights reserved.
+//
+
+import UIKit
+
+class HabitacionViewController: UIViewController {
+
+    
+    @IBOutlet weak var Letrero1: UILabel!
+    
+    @IBAction func ApagadorH(_ sender: UISwitch) {
+        
+        if (sender.isOn == true){
+            print("1")
+            Letrero1.text = "Encendidas ☀️    🙂"
+            
+        }
+        else{
+            print("0")
+            Letrero1.text = "Apagadas 🌘      🌙"
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        Letrero1.text = "Apagadas "
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
